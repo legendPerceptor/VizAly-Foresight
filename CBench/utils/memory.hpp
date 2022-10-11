5df4bb66aa7a880e3f088514387e1a6ebe000e84
Yuanjian Liu <achievinglegend@gmail.com>
2022-10-11 10:54:01 -0500

    Replace all tabs with four spaces

--- CBench/utils/memory.hpp
+++ CBench/utils/memory.hpp
@@ -32,178 +32,178 @@ Authors:
 
 class Memory
 {
-	unsigned long before_size, usage_size;	// program size
-	unsigned long before_rss,  usage_rss;	// resident set size
+    unsigned long before_size, usage_size;    // program size
+    unsigned long before_rss,  usage_rss;    // resident set size
 
-	void GetMemorySize(unsigned long &size, unsigned long &rss);
+    void GetMemorySize(unsigned long &size, unsigned long &rss);
 
 public:
-	Memory();
-	Memory(bool start);
-	~Memory() {};
+    Memory();
+    Memory(bool start);
+    ~Memory() {};
 
-	void start();
-	void stop();
-
-
-	unsigned long getMemorySizeInB() { return usage_size; }
-	double getMemorySizeInKB() { return usage_size / 1024.0; }
-	double getMemorySizeInMB() { return usage_size / (1024.0 * 1024.0); }
-
-	double getMemoryInUseInB();
-	double getMemoryInUseInKB();
-	double getMemoryInUseInMB();
+    void start();
+    void stop();
 
 
-	unsigned long getMemoryRSSInB() { return usage_rss; }
-	double getMemoryRSSInKB() { return usage_rss / 1024.0; }
-	double getMemoryRSSInMB() { return usage_rss / (1024.0 * 1024.0); }
+    unsigned long getMemorySizeInB() { return usage_size; }
+    double getMemorySizeInKB() { return usage_size / 1024.0; }
+    double getMemorySizeInMB() { return usage_size / (1024.0 * 1024.0); }
 
-	// static data and methods
-	static std::map<std::string, size_t> sizeOf;
+    double getMemoryInUseInB();
+    double getMemoryInUseInKB();
+    double getMemoryInUseInMB();
 
-	static bool allocate(void*& data, std::string datatype, size_t num_elems=1, int offset=0);
-	static bool release(void*& data, std::string datatype);
+
+    unsigned long getMemoryRSSInB() { return usage_rss; }
+    double getMemoryRSSInKB() { return usage_rss / 1024.0; }
+    double getMemoryRSSInMB() { return usage_rss / (1024.0 * 1024.0); }
+
+    // static data and methods
+    static std::map<std::string, size_t> sizeOf;
+
+    static bool allocate(void*& data, std::string datatype, size_t num_elems=1, int offset=0);
+    static bool release(void*& data, std::string datatype);
 };
 
 /* static*/ std::map<std::string, size_t> Memory::sizeOf = {
-	{   "int", sizeof(int)},
-	{   "float", sizeof(float)},
-	{  "double", sizeof(double)},
-	{  "int8_t", sizeof(int8_t)},
-	{ "int16_t", sizeof(int16_t)},
-	{ "int32_t", sizeof(int32_t)},
-	{ "int64_t", sizeof(int64_t)},
-	{ "uint8_t", sizeof(uint8_t)},
-	{"uint16_t", sizeof(uint16_t)},
-	{"uint32_t", sizeof(uint32_t)},
-	{"uint64_t", sizeof(uint64_t)}
+    {   "int", sizeof(int)},
+    {   "float", sizeof(float)},
+    {  "double", sizeof(double)},
+    {  "int8_t", sizeof(int8_t)},
+    { "int16_t", sizeof(int16_t)},
+    { "int32_t", sizeof(int32_t)},
+    { "int64_t", sizeof(int64_t)},
+    { "uint8_t", sizeof(uint8_t)},
+    {"uint16_t", sizeof(uint16_t)},
+    {"uint32_t", sizeof(uint32_t)},
+    {"uint64_t", sizeof(uint64_t)}
 };
 
 inline Memory::Memory()
 {
-	before_size = usage_size = 0;
-	before_rss  = usage_rss  = 0;
+    before_size = usage_size = 0;
+    before_rss  = usage_rss  = 0;
 }
 
 
 inline Memory::Memory(bool start)
 {
-	before_size = usage_size = 0;
-	before_rss  = usage_rss  = 0;
+    before_size = usage_size = 0;
+    before_rss  = usage_rss  = 0;
 
-	GetMemorySize(before_size, before_rss);
+    GetMemorySize(before_size, before_rss);
 }
 
 
 
 inline void Memory::start()
 {
-	GetMemorySize(before_size, before_rss);
+    GetMemorySize(before_size, before_rss);
 }
 
 
 inline void Memory::stop()
 {
-	unsigned long after_size, after_rss;
-	GetMemorySize(after_size, after_rss);
+    unsigned long after_size, after_rss;
+    GetMemorySize(after_size, after_rss);
 
-	usage_size = after_size - before_size;
-	usage_rss = after_rss - before_rss;
+    usage_size = after_size - before_size;
+    usage_rss = after_rss - before_rss;
 }
 
 
 inline bool Memory::allocate(void*& data, std::string datatype, size_t num_elems, int offset) 
 {
-	if (datatype == "int")
-		data = new int[num_elems + offset];
-	else if (datatype == "float")
-		data = new float[num_elems + offset];
-	else if (datatype == "double")
-		data = new double[num_elems + offset];
-	else if (datatype == "int8_t")
-		data = new int8_t[num_elems + offset];
-	else if (datatype == "int16_t")
-		data = new int16_t[num_elems + offset];
-	else if (datatype == "int32_t")
-		data = new int32_t[num_elems + offset];
-	else if (datatype == "int64_t")
-		data = new int64_t[num_elems + offset];
-	else if (datatype == "uint8_t")
-		data = new uint8_t[num_elems + offset];
-	else if (datatype == "uint16_t")
-		data = new uint16_t[num_elems + offset];
-	else if (datatype == "uint32_t")
-		data = new uint32_t[num_elems + offset];
-	else if (datatype == "uint64_t")
-		data = new uint64_t[num_elems + offset];
-	else
-		return false;
+    if (datatype == "int")
+        data = new int[num_elems + offset];
+    else if (datatype == "float")
+        data = new float[num_elems + offset];
+    else if (datatype == "double")
+        data = new double[num_elems + offset];
+    else if (datatype == "int8_t")
+        data = new int8_t[num_elems + offset];
+    else if (datatype == "int16_t")
+        data = new int16_t[num_elems + offset];
+    else if (datatype == "int32_t")
+        data = new int32_t[num_elems + offset];
+    else if (datatype == "int64_t")
+        data = new int64_t[num_elems + offset];
+    else if (datatype == "uint8_t")
+        data = new uint8_t[num_elems + offset];
+    else if (datatype == "uint16_t")
+        data = new uint16_t[num_elems + offset];
+    else if (datatype == "uint32_t")
+        data = new uint32_t[num_elems + offset];
+    else if (datatype == "uint64_t")
+        data = new uint64_t[num_elems + offset];
+    else
+        return false;
 
-	return true;
+    return true;
 }
 
 
 inline bool Memory::release(void*& data, std::string datatype) 
 {
 
-	if (data == nullptr) // already deallocated!
-		return true;
+    if (data == nullptr) // already deallocated!
+        return true;
 
-	if (datatype == "int")
-		delete[](int *) data;
-	else if (datatype == "float")
-		delete[](float *) data;
-	else if (datatype == "double")
-		delete[](double *) data;
-	else if (datatype == "int8_t")
-		delete[](int8_t *) data;
-	else if (datatype == "int16_t")
-		delete[](int16_t *) data;
-	else if (datatype == "int32_t")
-		delete[](int32_t *) data;
-	else if (datatype == "int64_t")
-		delete[](int64_t *) data;
-	else if (datatype == "uint8_t")
-		delete[](uint8_t *) data;
-	else if (datatype == "uint16_t")
-		delete[](uint16_t *) data;
-	else if (datatype == "uint32_t")
-		delete[](uint32_t *) data;
-	else if (datatype == "uint64_t")
-		delete[](uint64_t *) data;
-	else
-		return false;
+    if (datatype == "int")
+        delete[](int *) data;
+    else if (datatype == "float")
+        delete[](float *) data;
+    else if (datatype == "double")
+        delete[](double *) data;
+    else if (datatype == "int8_t")
+        delete[](int8_t *) data;
+    else if (datatype == "int16_t")
+        delete[](int16_t *) data;
+    else if (datatype == "int32_t")
+        delete[](int32_t *) data;
+    else if (datatype == "int64_t")
+        delete[](int64_t *) data;
+    else if (datatype == "uint8_t")
+        delete[](uint8_t *) data;
+    else if (datatype == "uint16_t")
+        delete[](uint16_t *) data;
+    else if (datatype == "uint32_t")
+        delete[](uint32_t *) data;
+    else if (datatype == "uint64_t")
+        delete[](uint64_t *) data;
+    else
+        return false;
 
-	data = nullptr;
-	return true;
+    data = nullptr;
+    return true;
 }
 
 
 inline double Memory::getMemoryInUseInB()
 {
-	unsigned long after_size, after_rss;
-	GetMemorySize(after_size, after_rss);
+    unsigned long after_size, after_rss;
+    GetMemorySize(after_size, after_rss);
 
-	return (after_size - before_size);
+    return (after_size - before_size);
 }
 
 
 inline double Memory::getMemoryInUseInKB()
 {
-	unsigned long after_size, after_rss;
-	GetMemorySize(after_size, after_rss);
+    unsigned long after_size, after_rss;
+    GetMemorySize(after_size, after_rss);
 
-	return (after_size - before_size) / (1024.0);
+    return (after_size - before_size) / (1024.0);
 }
 
 
 inline double Memory::getMemoryInUseInMB()
 {
-	unsigned long after_size, after_rss;
-	GetMemorySize(after_size, after_rss);
+    unsigned long after_size, after_rss;
+    GetMemorySize(after_size, after_rss);
 
-	return (after_size - before_size) / (1024.0 * 1024.0);
+    return (after_size - before_size) / (1024.0 * 1024.0);
 }
 
 
@@ -211,49 +211,49 @@ inline double Memory::getMemoryInUseInMB()
 // From VisIt avt/Pipeline/Pipeline/avtMemory.cpp
 inline void Memory::GetMemorySize(unsigned long &size, unsigned long &rss)
 {
-	size = 0;
-	rss  = 0;
+    size = 0;
+    rss  = 0;
 
-	FILE *file = fopen("/proc/self/statm", "r");
-	if (file == NULL)
-		return;
+    FILE *file = fopen("/proc/self/statm", "r");
+    if (file == NULL)
+        return;
 
-	int count = fscanf(file, "%lu%lu", &size, &rss);
-	if (count != 2)
-	{
-		fclose(file);
-		return;
-	}
-	size *= (unsigned long)getpagesize();
-	rss  *= (unsigned long)getpagesize();
-	fclose(file);
+    int count = fscanf(file, "%lu%lu", &size, &rss);
+    if (count != 2)
+    {
+        fclose(file);
+        return;
+    }
+    size *= (unsigned long)getpagesize();
+    rss  *= (unsigned long)getpagesize();
+    fclose(file);
 }
 #elif defined(WIN32)
 inline void Memory::GetMemorySize(unsigned long &size, unsigned long &rss)
 {
-	//Virtual Memory by current process
-	PROCESS_MEMORY_COUNTERS_EX pmc;
-	GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
-	SIZE_T virtualMemUsedByMe = pmc.PrivateUsage;
+    //Virtual Memory by current process
+    PROCESS_MEMORY_COUNTERS_EX pmc;
+    GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&pmc, sizeof(pmc));
+    SIZE_T virtualMemUsedByMe = pmc.PrivateUsage;
 
-	//Physical Memory Used by Current Process
-	SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
+    //Physical Memory Used by Current Process
+    SIZE_T physMemUsedByMe = pmc.WorkingSetSize;
 
-	size = virtualMemUsedByMe;
-	rss = physMemUsedByMe;
+    size = virtualMemUsedByMe;
+    rss = physMemUsedByMe;
 }
 #elif defined(__APPLE__) && defined(__MACH__)
 inline void Memory::GetMemorySize(unsigned long &size, unsigned long &rss)
 {
-	struct mach_task_basic_info info;
-	mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
-	if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
-									(task_info_t)&info, &infoCount ) != KERN_SUCCESS )
-		rss = (size_t)0L;		/* Can't access? */
+    struct mach_task_basic_info info;
+    mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+    if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
+                                    (task_info_t)&info, &infoCount ) != KERN_SUCCESS )
+        rss = (size_t)0L;        /* Can't access? */
 
-	rss = (size_t) info.resident_size;
-	size = (size_t) info.virtual_size;
+    rss = (size_t) info.resident_size;
+    size = (size_t) info.virtual_size;
 }
 #endif
 
-#endif	// _MEM_H_2
+#endif    // _MEM_H_2
--- CBench/utils/utils.hpp
+++ CBench/utils/utils.hpp
@@ -23,8 +23,8 @@ Authors:
 #include <mpi.h>
 
 #ifdef _WIN32
-	#include <direct.h>
-	#define mkdir(a, b) _mkdir(a)
+    #include <direct.h>
+    #define mkdir(a, b) _mkdir(a)
 #endif
 
 #include "json.hpp"
@@ -37,28 +37,28 @@ struct stat finfo; // utility for folder checking
 inline int createFolder(std::string folderName)
 {
   #ifdef _WIN32
-	folderName = ".\\" + folderName;
+    folderName = ".\\" + folderName;
   #endif
 
-	int res = stat(folderName.c_str(), &finfo);
+    int res = stat(folderName.c_str(), &finfo);
 
-	if (finfo.st_mode & S_IFDIR)
-		return 1; // Directory already exists 
-	else if (res != 0)
-	{
-		const int dir_err = mkdir(folderName.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-		if (-1 == dir_err)
-		{
-			std::cout << "Could not create directory " << folderName << std::endl;
-			return 0;
-		}
-	}
-	else 
-	{
-		std::cout << folderName << " is not a directory!" << std::endl;
-		return 0;
-	}
-	return 1;
+    if (finfo.st_mode & S_IFDIR)
+        return 1; // Directory already exists 
+    else if (res != 0)
+    {
+        const int dir_err = mkdir(folderName.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+        if (-1 == dir_err)
+        {
+            std::cout << "Could not create directory " << folderName << std::endl;
+            return 0;
+        }
+    }
+    else 
+    {
+        std::cout << folderName << " is not a directory!" << std::endl;
+        return 0;
+    }
+    return 1;
 }
 
 
@@ -66,8 +66,8 @@ inline int createFolder(std::string folderName)
 // Checks if file exists
 inline bool fileExisits(char *filename) 
 {
-	std::ifstream ifs(filename);
-	return ifs.good();
+    std::ifstream ifs(filename);
+    return ifs.good();
 }
 
 
@@ -75,16 +75,16 @@ inline bool fileExisits(char *filename)
 // Returns whether or not number is power of two
 inline bool isPowerOfTwo(int n) 
 { 
-	if (n == 0) 
-		return 0; 
+    if (n == 0) 
+        return 0; 
 
-	while (n != 1) 
-	{ 
-		if (n%2 != 0) 
-			return 0; 
-		n = n/2; 
-	} 
-	return 1; 
+    while (n != 1) 
+    { 
+        if (n%2 != 0) 
+            return 0; 
+        n = n/2; 
+    } 
+    return 1; 
 } 
 
 
@@ -92,8 +92,8 @@ inline bool isPowerOfTwo(int n)
 // On an input string, extracts the filename at the end of the string
 inline std::string extractFileName(std::string inputString)
 {
-	std::size_t pos = inputString.find_last_of("/\\");
-	return inputString.substr(pos+1); 
+    std::size_t pos = inputString.find_last_of("/\\");
+    return inputString.substr(pos+1); 
 }
 
 
@@ -102,33 +102,33 @@ inline std::string extractFileName(std::string inputString)
 // MPI datatype
 inline MPI_Datatype getMPIType(std::string dataType)
 {
-	if (dataType == "double")
-		return MPI_DOUBLE;
-	else if (dataType == "float")
-		return MPI_FLOAT;
-	else if (dataType == "int")
-		return MPI_INT;
-	else if (dataType == "long")
-		return MPI_LONG;
-	else if (dataType == "int8_t")
-		return MPI_INT8_T;
-	else if (dataType == "int16_t")
-		return MPI_INT16_T;
-	else if (dataType == "int32_t")
-		return MPI_INT32_T;
-	else if (dataType == "int64_t")
-		return MPI_INT64_T;
-	else if (dataType == "uint8_t")
-		return MPI_UINT8_T;
-	else if (dataType == "uint16_t")
-		return MPI_UINT16_T;
-	else if (dataType == "uint32_t")
-		return MPI_UINT32_T;
-	else if (dataType == "uint64_t")
-		return MPI_UINT64_T;
-	else if (dataType == "size_t")
-		return MPI_UNSIGNED_LONG_LONG;
-	else
+    if (dataType == "double")
+        return MPI_DOUBLE;
+    else if (dataType == "float")
+        return MPI_FLOAT;
+    else if (dataType == "int")
+        return MPI_INT;
+    else if (dataType == "long")
+        return MPI_LONG;
+    else if (dataType == "int8_t")
+        return MPI_INT8_T;
+    else if (dataType == "int16_t")
+        return MPI_INT16_T;
+    else if (dataType == "int32_t")
+        return MPI_INT32_T;
+    else if (dataType == "int64_t")
+        return MPI_INT64_T;
+    else if (dataType == "uint8_t")
+        return MPI_UINT8_T;
+    else if (dataType == "uint16_t")
+        return MPI_UINT16_T;
+    else if (dataType == "uint32_t")
+        return MPI_UINT32_T;
+    else if (dataType == "uint64_t")
+        return MPI_UINT64_T;
+    else if (dataType == "size_t")
+        return MPI_UNSIGNED_LONG_LONG;
+    else
         return 0;
 }
 
@@ -136,32 +136,32 @@ inline MPI_Datatype getMPIType(std::string dataType)
 // Allocates memory based on the string size name, adds an offset
 inline int allocateMem(std::string dataType, size_t numElements, int offset, void *& data)
 {
-	if (dataType == "float")
-		data = new float[numElements + offset];
-	else if (dataType == "double")
-		data = new double[numElements + offset];
-	else if (dataType == "int")
-		data = new int[numElements + offset];
-	else if (dataType == "int8_t")
-		data = new int8_t[numElements + offset];
-	else if (dataType == "int16_t")
-		data = new int16_t[numElements + offset];
-	else if (dataType == "int32_t")
-		data = new int32_t[numElements + offset];
-	else if (dataType == "int64_t")
-		data = new int64_t[numElements + offset];
-	else if (dataType == "uint8_t")
-		data = new uint8_t[numElements + offset];
-	else if (dataType == "uint16_t")
-		data = new uint16_t[numElements + offset];
-	else if (dataType == "uint32_t")
-		data = new uint32_t[numElements + offset];
-	else if (dataType == "uint64_t")
-		data = new uint64_t[numElements + offset];
-	else
-		return 0;
+    if (dataType == "float")
+        data = new float[numElements + offset];
+    else if (dataType == "double")
+        data = new double[numElements + offset];
+    else if (dataType == "int")
+        data = new int[numElements + offset];
+    else if (dataType == "int8_t")
+        data = new int8_t[numElements + offset];
+    else if (dataType == "int16_t")
+        data = new int16_t[numElements + offset];
+    else if (dataType == "int32_t")
+        data = new int32_t[numElements + offset];
+    else if (dataType == "int64_t")
+        data = new int64_t[numElements + offset];
+    else if (dataType == "uint8_t")
+        data = new uint8_t[numElements + offset];
+    else if (dataType == "uint16_t")
+        data = new uint16_t[numElements + offset];
+    else if (dataType == "uint32_t")
+        data = new uint32_t[numElements + offset];
+    else if (dataType == "uint64_t")
+        data = new uint64_t[numElements + offset];
+    else
+        return 0;
 
-	return 1;
+    return 1;
 }
 
 
@@ -208,7 +208,7 @@ inline int deAllocateMem(std::string dataType, void *& data)
 inline int getDataypeSize(std::string dataType)
 {
     if (dataType == "float")
-       	return 4;
+           return 4;
     else if (dataType == "double")
         return 8;
     else if (dataType == "int")
@@ -237,22 +237,22 @@ inline int getDataypeSize(std::string dataType)
 template <class T>
 inline void minMax(T * data, size_t numElements, T &minVal, T &maxVal, T &avg)
 {
-	minVal =  data[0];
-	maxVal =  data[0];
-	T sum = data[0];
+    minVal =  data[0];
+    maxVal =  data[0];
+    T sum = data[0];
 
-	for (size_t i=1; i<numElements; i++)
-	{
-	    T value = data[i];
+    for (size_t i=1; i<numElements; i++)
+    {
+        T value = data[i];
 
-		if (minVal > value)
-			minVal = value;
+        if (minVal > value)
+            minVal = value;
 
-		if (maxVal < value)
-			maxVal = value;
-	}
+        if (maxVal < value)
+            maxVal = value;
+    }
 
-	avg = sum/numElements;
+    avg = sum/numElements;
 }
 
 
@@ -260,94 +260,94 @@ inline void minMax(T * data, size_t numElements, T &minVal, T &maxVal, T &avg)
 // Function to validate whatever could be wrong with the input
 inline int validateInput(int argc, char *argv[], int myRank, int numRanks)
 {
-	// Check if we have the right number of arguments
-	if (argc < 2)
-	{
-		if (myRank == 0)
-		{
-			std::cerr << "Input argument needed. Run as: <path to input JSON file>" << std::endl;
-			std::cerr << "Read arguments: " << argc << std::endl;
-		}
+    // Check if we have the right number of arguments
+    if (argc < 2)
+    {
+        if (myRank == 0)
+        {
+            std::cerr << "Input argument needed. Run as: <path to input JSON file>" << std::endl;
+            std::cerr << "Read arguments: " << argc << std::endl;
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
-	// Check if input file provided exists
-	if (!fileExisits(argv[1]))
-	{
-		if (myRank == 0)
-			std::cerr << "Could not find input JSON file " << argv[1] << "." << std::endl;
+    // Check if input file provided exists
+    if (!fileExisits(argv[1]))
+    {
+        if (myRank == 0)
+            std::cerr << "Could not find input JSON file " << argv[1] << "." << std::endl;
 
-		return 0;
-	}
-
-
-
-	// Validate JSON file
-	nlohmann::json jsonInput;
-	std::ifstream jsonFile(argv[1]);
-
-	try
-	{
-		jsonFile >> jsonInput;
-	}
-	catch (nlohmann::json::parse_error &e)
-	{
-		if (myRank == 0)
-			std::cerr << "Input JSON file " << argv[1] << " is invalid!\n"
-					  << e.what() << "\n"
-					  << "Validate your JSON file using e.g. https://jsonformatter.curiousconcept.com/ " << std::endl;
-		return 0;
-	}
+        return 0;
+    }
 
 
 
-	// Check if powers of 2 number of ranks
-	if (jsonInput["cbench"]["output"].find("output-decompressed-location") != jsonInput["cbench"]["output"].end())
-		if (!isPowerOfTwo(numRanks))
-		{
-			if (myRank == 0)
-				std::cerr << "Please run with powers of 2 number of ranks. e.g. 4, 8, 16, 32, ... when writing out HACC files."
-						  << std::endl;
+    // Validate JSON file
+    nlohmann::json jsonInput;
+    std::ifstream jsonFile(argv[1]);
 
-			return 0;
-		}
+    try
+    {
+        jsonFile >> jsonInput;
+    }
+    catch (nlohmann::json::parse_error &e)
+    {
+        if (myRank == 0)
+            std::cerr << "Input JSON file " << argv[1] << " is invalid!\n"
+                      << e.what() << "\n"
+                      << "Validate your JSON file using e.g. https://jsonformatter.curiousconcept.com/ " << std::endl;
+        return 0;
+    }
 
-	return 1;
+
+
+    // Check if powers of 2 number of ranks
+    if (jsonInput["cbench"]["output"].find("output-decompressed-location") != jsonInput["cbench"]["output"].end())
+        if (!isPowerOfTwo(numRanks))
+        {
+            if (myRank == 0)
+                std::cerr << "Please run with powers of 2 number of ranks. e.g. 4, 8, 16, 32, ... when writing out HACC files."
+                          << std::endl;
+
+            return 0;
+        }
+
+    return 1;
 }
 
 //
 // creates a .py file that will plot a histogram for a specific metric using matplotlib
 inline std::string python_histogram(size_t numBins, float min_val, float max_val, std::vector<float> histogram)
 {
-	std::stringstream outputFileSS;
-	outputFileSS << "import sys" << std::endl;
-	outputFileSS << "import numpy as np" << std::endl;
-	outputFileSS << "import matplotlib" << std::endl;
-	outputFileSS << "matplotlib.use(\'agg\')" << std::endl;
-	outputFileSS << "import matplotlib.pyplot as plt" << std::endl;
+    std::stringstream outputFileSS;
+    outputFileSS << "import sys" << std::endl;
+    outputFileSS << "import numpy as np" << std::endl;
+    outputFileSS << "import matplotlib" << std::endl;
+    outputFileSS << "matplotlib.use(\'agg\')" << std::endl;
+    outputFileSS << "import matplotlib.pyplot as plt" << std::endl;
 
-	outputFileSS << "y=[";
-	std::size_t i;
-	for (i = 0; i < numBins - 1; ++i)
-		outputFileSS << std::to_string(histogram[i]) << ", ";
-	outputFileSS << std::to_string(histogram[i]) << "]" << std::endl;
+    outputFileSS << "y=[";
+    std::size_t i;
+    for (i = 0; i < numBins - 1; ++i)
+        outputFileSS << std::to_string(histogram[i]) << ", ";
+    outputFileSS << std::to_string(histogram[i]) << "]" << std::endl;
 
-	outputFileSS << "minVal=" << std::to_string(min_val) << std::endl;
-	outputFileSS << "maxVal=" << std::to_string(max_val) << std::endl;
-	outputFileSS << "plotName=sys.argv[0]" << std::endl;
-	outputFileSS << "plotName = plotName.replace('.py','.png')" << std::endl;
+    outputFileSS << "minVal=" << std::to_string(min_val) << std::endl;
+    outputFileSS << "maxVal=" << std::to_string(max_val) << std::endl;
+    outputFileSS << "plotName=sys.argv[0]" << std::endl;
+    outputFileSS << "plotName = plotName.replace('.py','.png')" << std::endl;
 
-	outputFileSS << "numVals = len(y)" << std::endl;
-	outputFileSS << "x = np.linspace(minVal, maxVal, numVals+1)[1:]" << std::endl;
-	outputFileSS << "plt.plot(x,y, linewidth=0.5)" << std::endl;
-	outputFileSS << "plt.title(plotName)" << std::endl;
-	outputFileSS << "plt.yscale(\"linear\") #log,linear,symlog,logit" << std::endl;
-	outputFileSS << "plt.ylabel(\"Frequency\")" << std::endl;
-	outputFileSS << "plt.xticks(rotation=90)" << std::endl;
-	outputFileSS << "plt.tight_layout()" << std::endl;
-	outputFileSS << "plt.savefig(plotName, dpi=300)" << std::endl;
+    outputFileSS << "numVals = len(y)" << std::endl;
+    outputFileSS << "x = np.linspace(minVal, maxVal, numVals+1)[1:]" << std::endl;
+    outputFileSS << "plt.plot(x,y, linewidth=0.5)" << std::endl;
+    outputFileSS << "plt.title(plotName)" << std::endl;
+    outputFileSS << "plt.yscale(\"linear\") #log,linear,symlog,logit" << std::endl;
+    outputFileSS << "plt.ylabel(\"Frequency\")" << std::endl;
+    outputFileSS << "plt.xticks(rotation=90)" << std::endl;
+    outputFileSS << "plt.tight_layout()" << std::endl;
+    outputFileSS << "plt.savefig(plotName, dpi=300)" << std::endl;
 
-	return outputFileSS.str();
+    return outputFileSS.str();
 }
 
